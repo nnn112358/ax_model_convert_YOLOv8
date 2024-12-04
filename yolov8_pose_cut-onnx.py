5497930188e764f9a8e022dfc4f76aa7c5c55d31
nnn <27625496+nnn112358@users.noreply.github.com>
2024-12-04 18:41:35 +0900

    Add files via upload

---
+++ yolov8_pose_cut-onnx.py
@@ -0,0 +1,22 @@
+import onnx
+import os
+
+def extract_onnx_model(input_path, output_path):
+   
+   input_names = ["images"]
+   output_names = [
+       "/model.22/Concat_1_output_0",
+       "/model.22/Concat_2_output_0", 
+       "/model.22/Concat_3_output_0",
+       "/model.22/cv4.0/cv4.0.2/Conv_output_0",
+       "/model.22/cv4.1/cv4.1.2/Conv_output_0",
+       "/model.22/cv4.2/cv4.2.2/Conv_output_0"
+   ]
+
+   onnx.utils.extract_model(input_path, output_path, input_names, output_names)
+
+# Usage
+os.chdir('./model')
+extract_onnx_model("yolov8n-pose.onnx", "yolov8n-pose-cut.onnx")
+extract_onnx_model("yolov8s-pose.onnx", "yolov8s-pose-cut.onnx")
+extract_onnx_model("yolov8m-pose.onnx", "yolov8m-pose-cut.onnx")
---
+++ yolov8_seg_cut-onnx.py
@@ -0,0 +1,23 @@
+import onnx
+import os
+
+def extract_onnx_model(input_path, output_path):
+   
+   input_names = ["images"]
+   output_names = [
+       "/model.22/Concat_1_output_0",
+       "/model.22/Concat_2_output_0", 
+       "/model.22/Concat_3_output_0",
+       "/model.22/cv4.0/cv4.0.2/Conv_output_0",
+       "/model.22/cv4.1/cv4.1.2/Conv_output_0",
+       "/model.22/cv4.2/cv4.2.2/Conv_output_0",
+		"output1"
+   ]
+
+   onnx.utils.extract_model(input_path, output_path, input_names, output_names)
+
+# Usage
+os.chdir('./model')
+extract_onnx_model("yolov8n-seg.onnx", "yolov8n-seg-cut.onnx")
+extract_onnx_model("yolov8s-seg.onnx", "yolov8s-seg-cut.onnx")
+extract_onnx_model("yolov8m-seg.onnx", "yolov8m-seg-cut.onnx")
